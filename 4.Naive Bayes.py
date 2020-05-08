5ff83128dd44a15a4629e948ecf7b925663de864
Yan625 <55767589+Yan625@users.noreply.github.com>
2020-05-08 17:16:29 -0400

    Add files via upload

---
+++ 4.Naive Bayes.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+
+def fit(X_train, Y_train):
+    result = {}
+    class_values = set(Y_train)
+    for current_class in class_values:
+        result[current_class] = {}
+        result["total_data"] = len(Y_train)
+        current_class_rows = (Y_train == current_class)
+        X_train_current = X_train[current_class_rows]
+        Y_train_current = Y_train[current_class_rows]
+        num_features = X_train.shape[1]
+        result[current_class]["total_count"] = len(Y_train_current)
+        for j in range(1, num_features + 1):
+            result[current_class][j] = {}
+            all_possible_values = set(X_train[:, j - 1])
+            for current_value in all_possible_values:
+                result[current_class][j][current_value] = (X_train_current[:, j - 1] == current_value).sum()
+                
+    return result
+
+
+def probability(dictionary, x, current_class):
+    output = np.log(dictionary[current_class]["total_count"]) - np.log(dictionary["total_data"])
+    num_features = len(dictionary[current_class].keys()) - 1;
+    for j in range(1, num_features + 1):
+        xj = x[j - 1]
+        count_current_class_with_value_xj = dictionary[current_class][j][xj] + 1
+        count_current_class = dictionary[current_class]["total_count"] + len(dictionary[current_class][j].keys())
+        current_xj_probablity = np.log(count_current_class_with_value_xj) - np.log(count_current_class)
+        output = output + current_xj_probablity
+    return output
+
+
+def predictSinglePoint(dictionary, x):
+    classes = dictionary.keys()
+    best_p = -1000
+    best_class = -1
+    first_run = True
+    for current_class in classes:
+        if (current_class == "total_data"):
+            continue
+        p_current_class = probability(dictionary, x, current_class)
+        if (first_run or p_current_class > best_p):
+            best_p = p_current_class
+            best_class = current_class
+        first_run = False
+    return best_class
+
+def predict(dictionary, X_test):
+    y_pred = []
+    for x in X_test:
+        x_class = predictSinglePoint(dictionary, x)
+        y_pred.append(x_class)
+    return y_pred
+
+#deal with continues columns
+def makeLabelled(column):
+    second_limit = column.mean()
+    first_limit = 0.5 * second_limit
+    third_limit = 1.5*second_limit
+    for i in range (0,len(column)):
+        if (column[i] < first_limit):
+            column[i] = 0
+        elif (column[i] < second_limit):
+            column[i] = 1
+        elif(column[i] < third_limit):
+            column[i] = 2
+        else:
+            column[i] = 3
+    return column
+
